1226c5e826c6f7d99396719477862b4c1d0c0143
Alex-gikungu <alexigikungu.012@gmail.com>
2023-06-11 22:18:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function showTab(tabId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(function(section) {
+      if (section.id === tabId) {
+        section.style.display = 'block';
+      } else {
+        section.style.display = 'none';
+      }
+    });
+  }
+
+let submitBtn = document.querySelector('#submitBtn');
+submitBtn.addEventListener('click', function() {
+  console.log(alert`submited`);
+});
+
+let whatsappIcon = document.querySelector('#whatsapp-icon');
+whatsappIcon.addEventListener('click', function() {
+  window.open('https://wa.me/whatsappphonenumber', '_blank');
+});
+
+let facebookIcon = document.querySelector('#facebook-icon');
+facebookIcon.addEventListener('click',function (){
+  window.open('https://www.facebook.com/Alexi Alex"_blank"')
+});
+
+
+let twitterIcon = document.querySelector('#twitter');
+twitterIcon.addEventListener('click',function(){
+  window.open('https://twitter.com/@AlexiGikun71556 "_blank"')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
